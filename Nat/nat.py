ad1f4f858b4d6dd0f80ca1b6e9a7267ee9f0733b
Ohad Eytan <ohadey@gmail.com>
2017-08-09 07:54:19 -0400

    nat works

---
+++ Nat/nat.py
@@ -0,0 +1,43 @@
+from pydhcplib.dhcp_packet import *
+from pydhcplib.dhcp_network import *
+import socket
+import select
+from struct import unpack
+from struct import pack
+from time import sleep
+
+# create a socket object
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+# AWS Public IP
+host = '34.194.147.243'
+port = 9999
+# connection to hostname on the port.
+server.connect((host, port))                               
+
+socket_array = [server]
+for i in range(1,301):
+    c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+    c.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
+    c.setblocking(0)
+    c.bind(('0.0.0.0', 50000+i))
+    socket_array.append(c)
+
+while True:
+    readable, writable, exceptional = select.select(socket_array, [], [], 30)
+    for s in readable:
+        if s is server:
+            data_raw = s.recv(249, socket.MSG_WAITALL)
+            if len(data_raw) < 249:
+                sleep(1)
+                continue
+            packet, port = data_raw[:244], data_raw[-5:]
+            c.sendto(packet, ('255.255.255.255', int(port)+10000))
+        elif s in socket_array:
+            packet = s.recv(244)
+            extPacket = packet + str(s.getsockname()[1])
+            server.sendall(extPacket)
+        else:
+            print("other socket")                           
+
+server.close()
+c.close()
